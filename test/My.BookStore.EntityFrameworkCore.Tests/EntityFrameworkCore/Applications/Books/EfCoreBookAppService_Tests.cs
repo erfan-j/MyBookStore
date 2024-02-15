a4b9243dfdde16ddfe5accc1a6dcc81f0e894a5e
Saanaa.user1 <saanaa.user1@gmail.com>
2024-02-15 11:40:59 +0330

    feat: test operations added to test solution.

---
+++ test/My.BookStore.EntityFrameworkCore.Tests/EntityFrameworkCore/Applications/Books/EfCoreBookAppService_Tests.cs
@@ -0,0 +1,10 @@
+﻿using My.BookStore.Books;
+using Xunit;
+
+namespace My.BookStore.EntityFrameworkCore.Applications.Books;
+
+[Collection(BookStoreTestConsts.CollectionDefinitionName)]
+public class EfCoreBookAppService_Tests : BookAppService_Tests<BookStoreEntityFrameworkCoreTestModule>
+{
+
+}
