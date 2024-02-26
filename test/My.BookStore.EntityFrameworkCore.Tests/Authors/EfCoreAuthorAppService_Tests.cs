1aa3a766f64353aac2b5b9ca4231735feb6fc2be
erfan-j <erfan.jahandoost84@gmail.com>
2024-02-26 21:42:58 +0330

    feat: AuthorTests added.

---
+++ test/My.BookStore.EntityFrameworkCore.Tests/Authors/EfCoreAuthorAppService_Tests.cs
@@ -0,0 +1,10 @@
+﻿using My.BookStore.EntityFrameworkCore;
+using Xunit;
+
+namespace My.BookStore.Authors;
+
+[Collection(BookStoreTestConsts.CollectionDefinitionName)]
+public class EfCoreAuthorAppService_Tests : AuthorAppService_Tests<BookStoreEntityFrameworkCoreTestModule>
+{
+
+}
